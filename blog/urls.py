5e4dfee9871fe619467510a7e832085886ea5998
mukmukmukmuk <ksm010825@naver.com>
2023-10-27 10:39:24 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('category/<str:slug>/',views.category_page),
     path('',views.PostList.as_view()), #blog.urls.py로 위임
     path('<int:pk>/',views.PostDetail.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView,DetailView
 from .models import Post,Category
-
+from django.shortcuts import render
 #def index(request):
 #    posts=Post.objects.all().order_by('-pk')
 #    return render(
@@ -11,6 +11,25 @@ from .models import Post,Category
 #
 #        }
 #    )
+
+def category_page(request,slug):
+    if slug=='no_category':
+        category = '미분류'
+        post_list=Post.objects.filter(category=None)
+    else:
+        category=Category.objects.get(slug=slug)
+        post_list=Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories':Category.objects.all(),
+            'no_category_post_count':Post.objects.filter(category=None).count(),
+            'category':category,
+        }
+    )
 
 class PostList(ListView):
     model=Post
