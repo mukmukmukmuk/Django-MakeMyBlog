52741a0aac8e2b7347b22dd0caf34e0f6c7e3629
mukmukmukmuk <ksm010825@naver.com>
2023-11-01 09:41:54 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,22 @@ def category_page(request,slug):
         }
     )
 
+def tag_page(request,slug):
+    tag=Tag.objects.get(slug=slug)
+    post_list=tag.post_set.all()
+    #post_list=Post.objects.filter(tags=tag)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'tag' : tag,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+        }
+    )
+
 class PostList(ListView):
     model=Post
     ordering = '-pk'
