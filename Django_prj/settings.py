24029d82f2fb7e5f4ad4f92edd8661df4322b1a8
mukmukmukmuk <ksm010825@naver.com>
2023-11-17 16:49:47 +0900

    allauth 완료

--- Django_prj/settings.py
+++ Django_prj/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
     'blog',
     'single_pages',
 ]
@@ -53,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'Django_prj.urls'
@@ -132,3 +139,18 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
+SITE_ID=1
+
+ACCOUNT_EMAIL_REQUIRED= True
+ACCOUNT_EMAIL_VERIFICATION='none'
--- Django_prj/urls.py
+++ Django_prj/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('blog/',include('blog.urls')), #blog.urls.py로 위임
     path('',include('single_pages.urls')),
     path('markdownx/',include('markdownx.urls')),
+    path('accounts/',include('allauth.urls')),
+
 ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
