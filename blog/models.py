89f8fdb236aee8bc7ebf3b037a2a179fd6fbd73d
mukmukmukmuk <ksm010825@naver.com>
2023-11-22 13:00:28 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -57,6 +57,11 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at=models.DateTimeField(auto_now=True)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1782/11c26ea9746f759a/svg/{{self.author.email}}'
     def __str__(self):
         return f'{self.author}::{self.content}'
     
