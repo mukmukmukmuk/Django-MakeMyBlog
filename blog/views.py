98dc0e739b153ff9ce2fc46ccc3330954bed8ca1
mukmukmukmuk <ksm010825@naver.com>
2023-11-28 10:05:42 +0900

    도커에 장고만 담기

--- blog/views.py
+++ blog/views.py
@@ -2,34 +2,36 @@ from typing import Any
 from django import http
 from django.db.models.query import QuerySet
 from django.http.response import HttpResponse
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
-from .models import Post,Category,Tag,Comment
-from django.shortcuts import render,redirect
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post, Category, Tag, Comment
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
-def delete_comment(request,pk):
-    comment=get_object_or_404(Comment,pk=pk)
-    post=comment.post
-    if request.user.is_authenticated and request.user==comment.author:
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
         comment.delete()
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
 
+
 def new_comment(request, pk):
     if request.user.is_authenticated:
-        post=get_object_or_404(Post,pk=pk)
-        if request.method == 'POST':
-            comment_form=CommentForm(request.POST)
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
-                comment=comment_form.save(commit=False)
-                comment.post=post
-                comment.author=request.user
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
                 comment.save()
                 return redirect(comment.get_absolute_url())
         else:
@@ -38,121 +40,127 @@ def new_comment(request, pk):
         raise PermissionDenied
 
 
-def category_page(request,slug):
-    if slug=='no_category':
-        category = '미분류'
-        post_list=Post.objects.filter(category=None)
+def category_page(request, slug):
+    if slug == "no_category":
+        category = "미분류"
+        post_list = Post.objects.filter(category=None)
     else:
-        category=Category.objects.get(slug=slug)
-        post_list=Post.objects.filter(category=category)
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list': post_list,
-            'categories':Category.objects.all(),
-            'no_category_post_count':Post.objects.filter(category=None).count(),
-            'category':category,
-        }
+            "post_list": post_list,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+            "category": category,
+        },
     )
 
-def tag_page(request,slug):
-    tag=Tag.objects.get(slug=slug)
-    post_list=tag.post_set.all()
-    #post_list=Post.objects.filter(tags=tag)
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    # post_list=Post.objects.filter(tags=tag)
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list' : post_list,
-            'tag' : tag,
-            'categories' : Category.objects.all(),
-            'no_category_post_count' : Post.objects.filter(category=None).count(),
-        }
+            "post_list": post_list,
+            "tag": tag,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+        },
     )
+
 
 class PostList(ListView):
-    model=Post
-    ordering = '-pk'
-    paginate_by=5
+    model = Post
+    ordering = "-pk"
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context=super().get_context_data()
-        context['categories']=Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context = super().get_context_data()
+        context["categories"] = Category.objects.all()
+        context["no_category_post_count"] = Post.objects.filter(category=None).count()
         return context
+
 
 class PostDetail(DetailView):
-    model=Post
+    model = Post
+
     def get_context_data(self, *, object_list=None, **kwargs):
-        context=super().get_context_data()
-        context['categories']=Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
-        context['comment_form']=CommentForm
+        context = super().get_context_data()
+        context["categories"] = Category.objects.all()
+        context["no_category_post_count"] = Post.objects.filter(category=None).count()
+        context["comment_form"] = CommentForm
         return context
 
-class PostCreate(LoginRequiredMixin,UserPassesTestMixin, CreateView):
-    model=Post
-    fields=['title','hook_text','content','head_image','file_upload','category']
 
-    def test_func(self):
-        return self.request.user.is_superuser or self.request.user.is_staff
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
 
     def form_valid(self, form):
-        current_user=self.request.user
-        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+        current_user = self.request.user
+        if current_user.is_authenticated:
             form.instance.author = current_user
-            response=super().form_valid(form)
+            response = super().form_valid(form)
 
-            tags=self.request.POST.get('tags')
+            tags = self.request.POST.get("tags")
             if tags:
-                tags_list = tags.strip().replace(',',';').split(';')
+                tags_list = tags.strip().replace(",", ";").split(";")
                 for t in tags_list:
-                    t=t.strip()
+                    t = t.strip()
                     tag, is_tag_created = Tag.objects.get_or_create(name=t)
                     if is_tag_created:
-                        tag.slug=slugify(t,allow_unicode=True)
+                        tag.slug = slugify(t, allow_unicode=True)
                         tag.save()
                     self.object.tags.add(tag)
             return response
         else:
-            return redirect('/blog/')
-    
-class PostUpdate(LoginRequiredMixin,UpdateView):
-    model = Post
-    fields = ['title','hook_text','content','head_image','file_upload','category']
+            return redirect("/blog/")
 
-    template_name='blog/post_update_form.html'
-    
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
+
+    template_name = "blog/post_update_form.html"
+
     def get_context_data(self, *, object_list=None, **kwargs):
-        context=super().get_context_data()
+        context = super().get_context_data()
         if self.object.tags.exists():
-            context['tags_str_default']= '; '.join([t.name for t in self.object.tags.all()])
+            context["tags_str_default"] = "; ".join(
+                [t.name for t in self.object.tags.all()]
+            )
         return context
 
-    def dispatch(self,request, *args: Any, **kwargs: Any) -> HttpResponse:
+    def dispatch(self, request, *args: Any, **kwargs: Any) -> HttpResponse:
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super().dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
-        
+
     def form_valid(self, form):
-        response=super().form_valid(form)
+        response = super().form_valid(form)
         self.object.tags.clear()
-        tags=self.request.POST.get('tags')
+        tags = self.request.POST.get("tags")
         if tags:
-            tags_list = tags.strip().replace(',',';').split(';')
+            tags_list = tags.strip().replace(",", ";").split(";")
             for t in tags_list:
-                t=t.strip()
+                t = t.strip()
                 tag, is_tag_created = Tag.objects.get_or_create(name=t)
                 if is_tag_created:
-                    tag.slug=slugify(t,allow_unicode=True)
+                    tag.slug = slugify(t, allow_unicode=True)
                     tag.save()
                 self.object.tags.add(tag)
         return response
-    
+
+
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
     form_class = CommentForm
@@ -162,19 +170,20 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
             return super().dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
-        
+
+
 class PostSearch(PostList):
-    paginate_by=None
+    paginate_by = None
 
     def get_queryset(self) -> QuerySet[Any]:
-        q=self.kwargs['q']
-        post_list=Post.objects.filter(
+        q = self.kwargs["q"]
+        post_list = Post.objects.filter(
             Q(title__contains=q) | Q(tags__name__contains=q)
         ).distinct()
         return post_list
-    
+
     def get_context_data(self, *, object_list=None, **kwargs):
-        context=super().get_context_data(object_list=object_list, **kwargs)
-        q=self.kwargs['q']
-        context['search_info'] = f'Search: {q} ({self.get_queryset().count()}개)'
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        q = self.kwargs["q"]
+        context["search_info"] = f"Search: {q} ({self.get_queryset().count()}개)"
         return context
