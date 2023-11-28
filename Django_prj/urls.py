6e90cf101e5f6224f9d2e01829268a9a1a48ef38
mukmukmukmuk <ksm010825@naver.com>
2023-11-28 11:40:54 +0900

    도커에 gunicorn 적용하기

--- Django_prj/urls.py
+++ Django_prj/urls.py
@@ -1,16 +1,15 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')), #blog.urls.py로 위임
-    path('',include('single_pages.urls')),
-    path('markdownx/',include('markdownx.urls')),
-    path('accounts/',include('allauth.urls')),
-
+    path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls")),  # blog.urls.py로 위임
+    path("", include("single_pages.urls")),
+    path("markdownx/", include("markdownx.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
